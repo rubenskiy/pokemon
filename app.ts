df94f2c09e556ab6a88670d6046fe43e49fb11ae
Ruben Croft <r.croft2734@gmail.com>
2024-06-26 17:29:00 +0100

    async

--- app.ts
+++ app.ts
@@ -1,1 +1,12 @@
-console.log("hello")
+const getPokemon = async (pokemonName) => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=800');
+    let data = await response.json()
+    console.log(data)
+    data
+
+    //.then(response => response.json())
+
+    
+}
+
+getPokemon('Bulbasaur')
