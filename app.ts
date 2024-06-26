09d5a0c3c9e9281fe7ba0f737bc09fdb513597e0
Ruben Croft <r.croft2734@gmail.com>
2024-06-26 19:27:23 +0100

    latest commit

--- app.ts
+++ app.ts
@@ -1,12 +1,10 @@
-const getPokemon = async (pokemonName) => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=800');
-    let data = await response.json()
-    console.log(data)
-    data
+// // const getPokemon = async (pokemonName) => {
+// //     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=800');
+// //     let data = await response.json()
+// //     console.log(data)
+// //     data
 
-    //.then(response => response.json())
+//     //.then(response => response.json()  
+// }
 
-    
-}
-
-getPokemon('Bulbasaur')
+// getPokemon('Bulbasaur')
